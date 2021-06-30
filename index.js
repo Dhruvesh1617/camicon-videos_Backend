522415ebc57aef3b22b0859dab705eab051ad7c1
Dhruvesh1617 <shettydhruvesh16@gmail.com>
2021-06-30 08:16:15 +0530

    update:cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express=require("express")
 const app=express();
+const cors=require("cors")
 const port=process.env.PORT || 5000;
 const {dbConnection}=require("./dbConnection/dbConnection")
 const {videoRouter}=require("./routes/videos")
+app.use(cors())
 app.use(express.json()) //to parse JSON-body
 dbConnection() //running connection
 
